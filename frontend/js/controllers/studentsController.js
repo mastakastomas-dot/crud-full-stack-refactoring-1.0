bd76fb506fbd925f105e92c19721c66269e448ff
juancruz2004 <juancruzmontesdeoca8@gmail.com>
2025-10-28 15:29:35 -0300

    Update studentsController.js

--- frontend/js/controllers/studentsController.js
+++ frontend/js/controllers/studentsController.js
@@ -220,12 +220,12 @@ async function confirmDelete(id)
     try 
     {
       let response;
-               response =  await studentsAPI.delete(id);             
-            if (response.error) {
-                throw new Error(response.error); // Si hubo un error pasa a catch
-            }
-    if (response.message){
-        await studentsAPI.remove(id);
+      response =  await studentsAPI.delete(id);             
+      if (response.error) {
+         throw new Error(response.error); // Si hubo un error pasa a catch
+     }
+      if (response.message){
+         await studentsAPI.remove(id);
          loadStudents();
      }
     }
