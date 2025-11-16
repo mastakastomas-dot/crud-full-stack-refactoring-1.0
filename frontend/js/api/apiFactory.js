24c71be8483e53556a1cbcf9e4fd495bb33fc00c
tomassacadura <148502251+tomassacadura@users.noreply.github.com>
2025-11-16 17:28:04 +0000

    Refactor manejo de materias: mejora en la validación y manejo de errores en el formulario de creación

--- frontend/js/api/apiFactory.js
+++ frontend/js/api/apiFactory.js
@@ -9,7 +9,7 @@
 */
 
 export function createAPI(moduleName, config = {}) {
-    const API_URL = config.urlOverride ?? `../../backend/server.php?module=${moduleName}`;
+    const API_URL = config.urlOverride ?? `../../../backend/server.php?module=${moduleName}`;
 
     async function sendJSON(method, data) {
         const res = await fetch(API_URL, {
